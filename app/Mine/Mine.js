c24ee39e59e0c1058dc3f360f5927de901aa83fa
Havi_li <1058695900@qq.com>
2016-03-30 14:31:48 +0800

    tabbar ok

---
+++ app/Mine/Mine.js
@@ -0,0 +1,23 @@
+'use strict';
+import React, {
+    StyleSheet,
+    View,
+    Text,
+
+} from 'react-native';
+
+class Mine extends React.Component {
+
+    render(){
+        return (
+            <View>
+
+            </View>
+        );
+    }
+
+}
+
+let styles = StyleSheet.create({});
+
+export default Mine;
--- index.android.js
+++ index.android.js
@@ -3,49 +3,110 @@
  * https://github.com/facebook/react-native
  */
 
+'use strict';
 import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Component,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TabBarIOS,
 } from 'react-native';
 
+import TabNavigator from 'react-native-tab-navigator';
+import Home from './app/Home/Home';
+import Merchant from './app/Merchant/Merchant';
+import Mine from './app/Mine/Mine';
+import Misc from './app/Misc/Misc';
+
+const SelectTab = {
+    Home:'home',
+    Merchant:'merchant',
+    Mine:'mine',
+    Misc:'misc'
+};
+
 class MeiTuan_DEMO extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    //进行初始化,初始化一些状态
+    // 构造
+    constructor(props) {
+        super(props);
+        // 初始状态
+        this.state = {
+            selectedTab:'home'
+        };
+    }
+
+    render() {
+        return (
+            <TabNavigator>
+                <TabNavigator.Item
+                    selected={this.state.selectedTab === 'home'}
+                    title="首页"
+                    renderIcon={() => <Image source={require('./app/images/icon_tabbar_mine.png')} style={styles.tabBarImage}/>}
+                    renderSelectedIcon={() => <Image source={require('./app/images/icon_tabbar_mine_selected.png')} style={styles.tabBarImage}/>}
+                    onPress={() => this.setState({ selectedTab: 'home' })}>
+                    {this._renderSubViews()}
+                </TabNavigator.Item>
+                <TabNavigator.Item
+                    selected={this.state.selectedTab === 'merchant'}
+                    title="商家"
+                    renderIcon={() => <Image source={require('./app/images/icon_tabbar_merchant_normal.png')} style={styles.tabBarImage}/>}
+                    renderSelectedIcon={() => <Image source={require('./app/images/icon_tabbar_merchant_selected.png')} style={styles.tabBarImage}/>}
+                    onPress={() => this.setState({ selectedTab: 'merchant' })}>
+                    {this._renderSubViews()}
+                </TabNavigator.Item>
+                <TabNavigator.Item
+                    selected={this.state.selectedTab === 'mine'}
+                    title="我的"
+                    renderIcon={() => <Image source={require('./app/images/icon_tabbar_mine.png')} style={styles.tabBarImage}/>}
+                    renderSelectedIcon={() => <Image source={require('./app/images/icon_tabbar_mine_selected.png')} style={styles.tabBarImage}/>}
+                    onPress={() => this.setState({ selectedTab: 'mine' })}>
+                    {this._renderSubViews()}
+                </TabNavigator.Item>
+                <TabNavigator.Item
+                    selected={this.state.selectedTab === 'misc'}
+                    title="更多"
+                    renderIcon={() => <Image source={require('./app/images/icon_tabbar_misc.png')} style={styles.tabBarImage}/>}
+                    renderSelectedIcon={() => <Image source={require('./app/images/icon_tabbar_misc_selected.png')} style={styles.tabBarImage}/>}
+                    onPress={() => this.setState({ selectedTab: 'misc' })}>
+                    {this._renderSubViews()}
+                </TabNavigator.Item>
+            </TabNavigator>
+        );
+    }
+
+    _renderSubViews(){
+        switch (this.state.selectedTab){
+            case SelectTab.Home:{
+                return (<Home/>);
+            }
+            case SelectTab.Merchant:{
+                return (<Merchant/>);
+            }
+            case SelectTab.Mine:{
+                return (<Mine/>);
+
+            }
+            case SelectTab.Misc:{
+                return (<Misc/>);
+            }
+        }
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    tabBarImage:{
+        height:25,
+        width:25
+    }
 });
 
 AppRegistry.registerComponent('MeiTuan_DEMO', () => MeiTuan_DEMO);
